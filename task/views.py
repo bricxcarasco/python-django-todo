773565192fb5b40801236f0e6949578c7c34d10b
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-02-07 12:32:36 +0800

    Create a ajax json response in task view

--- task/views.py
+++ task/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from django.http import JsonResponse
+
+
+from .models import Task
+from .forms import TaskForm
 
 class TaskView(View):
     def get(self, request):
-        return render(request, 'task/task.html', context={'name': 'RED'})
+        tasks = list(Task.objects.values())
+
+        if request.is_ajax():
+            return JsonResponse({'tasks': tasks}, status=200)
+            
+        return render(request, 'task/task.html')
