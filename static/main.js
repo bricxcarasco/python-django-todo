036924f1de75f23406e15c91783cb719664e34b4
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-02-09 11:28:56 +0800

    Create post request for todo form

--- static/main.js
+++ static/main.js
@@ -1,3 +1,12 @@
+function sendRequest(url, method, data) {
+  var r = axios({
+    method: method,
+    url: url,
+    data: data,
+    xsrfCookieName: "csrftoken",
+  });
+}
+
 var app = new Vue({
   el: "#app",
   data: {
--- task/views.py
+++ task/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 
 from .models import Task
 from .forms import TaskForm
+
 
 class TaskView(View):
     def get(self, request):
@@ -13,5 +15,14 @@ class TaskView(View):
 
         if request.is_ajax():
             return JsonResponse({'tasks': tasks}, status=200)
-            
+
         return render(request, 'task/task.html')
+
+    def post(self, request):
+        bound_form = TaskForm(request.POST)
+
+        if bound_form.is_valid():
+            new_task = bound_form.save()
+            return JsonResponse({'task': model_to_dict(bound_form)}, status=200)
+
+        return redirect('tasks_list_url')
